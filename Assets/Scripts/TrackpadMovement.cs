7fd66a9bd63f1985f9de628f3dcead4931a039d3
Mariko1 <32982829+Mariko1@users.noreply.github.com>
2017-10-21 22:25:35 +0300

    3

--- Assets/Scripts/TrackpadMovement.cs
+++ Assets/Scripts/TrackpadMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TrackpadMovement : MonoBehaviour {
 
+	Vector2 touchVector;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,19 @@ public class TrackpadMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		/*if (GvrControllerInput.IsTouching) { //второй if - проверка на dead zone
+			Debug.Log("touch");
+			touchVector = GvrControllerInput.TouchPosCentered;
+			if ((touchVector.x < 0.3f && touchVector.x > -0.3f) && (touchVector.y < 0.3f && touchVector.y > -0.3f)) {
+				Debug.Log("dead");
+				return;
+			}
+			else {
+				Debug.Log(touchVector);
+			}
+		}*/
+		touchVector.x = 3f;
+		touchVector.y = 7f;
+		Debug.Log(touchVector);
 	}
 }

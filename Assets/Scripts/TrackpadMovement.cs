a269289c1f0398202b692f74bca993d2384d1fae
Mariko1 <32982829+Mariko1@users.noreply.github.com>
2017-10-22 00:57:45 +0300

    4

--- Assets/Scripts/TrackpadMovement.cs
+++ Assets/Scripts/TrackpadMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrackpadMovement : MonoBehaviour {
 
 	Vector2 touchVector;
+	Vector3 newPos;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,19 +13,25 @@ public class TrackpadMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		/*if (GvrControllerInput.IsTouching) { //второй if - проверка на dead zone
-			Debug.Log("touch");
+
+		if (GvrControllerInput.IsTouching) { 
+			//Debug.Log("touch");
 			touchVector = GvrControllerInput.TouchPosCentered;
-			if ((touchVector.x < 0.3f && touchVector.x > -0.3f) && (touchVector.y < 0.3f && touchVector.y > -0.3f)) {
-				Debug.Log("dead");
+			if ((Mathf.Abs(touchVector.x) < 0.3f) && (Mathf.Abs(touchVector.y) < 0.3f)) { //проверка на dead zone
+				//Debug.Log("dead");
 				return;
 			}
-			else {
-				Debug.Log(touchVector);
+			newPos = Vector3.ProjectOnPlane(Camera.main.transform.up, Vector3.up).normalized;
+			//if (touchVector.y > 0) {				
+			//	newPos.z += 0.2f;
+			//}
+			if (touchVector.x > 0) {
+				newPos.x += 0.2f;
 			}
-		}*/
-		touchVector.x = 3f;
-		touchVector.y = 7f;
-		Debug.Log(touchVector);
+			else {
+				newPos.x -= 0.2f;
+			}
+			transform.position = transform.TransformPoint(newPos);
+		}
 	}
 }
